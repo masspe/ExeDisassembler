592fc1fb4cc12002e4f3959fcd3e3e3b6f0cf91e
masspe <massimilianosperanzadev@gmail.com>
2018-08-26 18:03:45 +0200

    sections

--- ExeDisassembler/Form1.Designer.cs
+++ ExeDisassembler/Form1.Designer.cs
@@ -29,10 +29,24 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -41,18 +55,8 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 56);
+            this.panel1.Size = new System.Drawing.Size(1034, 56);
             this.panel1.TabIndex = 0;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 10F);
-            this.richTextBox1.Location = new System.Drawing.Point(0, 56);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(800, 394);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
             // 
             // button1
             // 
@@ -68,16 +72,130 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 56);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1034, 527);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1026, 498);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "CPU (.text)";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1020, 492);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 365);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = ".rdata";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(786, 359);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 365);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = ".data";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox3.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox3.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(792, 365);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.WordWrap = false;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.richTextBox4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(774, 468);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = ".rsrc";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox4.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox4.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(774, 468);
+            this.richTextBox4.TabIndex = 0;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.WordWrap = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 560);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1034, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(1034, 583);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "ExeDisassembler";
             this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -85,9 +203,18 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- ExeDisassembler/Form1.cs
+++ ExeDisassembler/Form1.cs
@@ -15,19 +15,35 @@ namespace ExeDisassembler
 {
     public partial class Form1 : Form
     {
+        PeFile _peFile;
+
         public Form1()
         {
             InitializeComponent();
         }
-        public byte[] FileToByteArray(string fileName, PeFile _peFile)
+        public byte[] FileToByteArray(string fileName, PeFile _peFile,int PAddress, int PSize)
         {
-            uint PAddress = 0;
-            uint PSize = 0;
+            
             byte[] buff = null;
             FileStream fs = new FileStream(fileName,
                                            FileMode.Open,
                                            FileAccess.Read);
             BinaryReader br = new BinaryReader(fs);
+           
+
+
+            br.ReadBytes(PAddress);
+            buff = br.ReadBytes(PSize);
+            return buff;
+        }
+
+        public void readTextSection()
+        {
+            uint PAddress = 0;
+            uint PSize = 0;
+            ArchitectureMode mode = ArchitectureMode.x86_32;
+            if (_peFile.Is64Bit)
+                mode = ArchitectureMode.x86_64;
             foreach (var sec in _peFile.ImageSectionHeaders)
             {
                 Name = PeNet.Utilities.FlagResolver.ResolveSectionName(sec.Name);
@@ -35,21 +51,83 @@ namespace ExeDisassembler
                 {
                     PAddress = sec.PointerToRawData;
                     PSize = sec.SizeOfRawData;
+
+                    byte[] b = FileToByteArray(openFileDialog1.FileName, _peFile, (int)PAddress, (int)PSize);
+
+                    var disasm = new Disassembler(b, mode, 0, true);
+                    // Disassemble each instruction and output to console
+                    richTextBox1.Text = "";
+                    List<SharpDisasm.Instruction> dis = disasm.DisassembleList();
+                    richTextBox1.Text = "";
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = dis.Count;
+                    for (int i = 0; i < dis.Count; i++)
+                    {
+                        richTextBox1.AppendText(string.Format("{0:x16}  ", (long)_peFile.ImageNtHeaders.OptionalHeader.ImageBase + sec.VirtualAddress + (long) dis[i].Offset) + dis[i].ToString() + "\n");
+                        progressBar1.Value = i;
+                    }
+                    progressBar1.Visible = false;
                     break;
                 }
 
             }
+           
+        }
 
+        public void readDataSection(string sname, RichTextBox r)
+        {
+            uint PAddress = 0;
+            uint PSize = 0;
+          
+            foreach (var sec in _peFile.ImageSectionHeaders)
+            {
+                Name = PeNet.Utilities.FlagResolver.ResolveSectionName(sec.Name);
+                r.Text = "";
+                if (Name == sname)
+                {
+                    PAddress = sec.PointerToRawData;
+                    PSize = sec.SizeOfRawData;
 
-            br.ReadBytes((int)PAddress);
-            buff = br.ReadBytes((int)PSize);
-            return buff;
+                    byte[] b = FileToByteArray(openFileDialog1.FileName, _peFile, (int)PAddress, (int)PSize);
+
+                    string hstring = "";
+                    string rstring = "";
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = b.Length;
+                    for (int i = 0; i < b.Length; i++)
+                    {
+                        hstring += string.Format("{0:x2}:", b[i]);
+                        if (b[i] > 0)
+                            if (b[i] != 13 && b[i] != 10)
+                                rstring += (char)b[i];
+                            else
+                                rstring += "";
+                        
+
+                        if ((i+1) % 16 == 0 )
+                        {
+                            r.AppendText(string.Format("{0:x16}  ", (long) _peFile.ImageNtHeaders.OptionalHeader.ImageBase + sec.VirtualAddress +  (i-16)) + hstring +"  "+ rstring + "\n");
+                            hstring = "";
+                            rstring = "";
+                        }
+                        progressBar1.Value = i;
+                    }
+                    progressBar1.Visible = false;
+
+                    break;
+                }
+
+            }
+            
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            ArchitectureMode mode = ArchitectureMode.x86_32;
+            
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                this.Cursor = Cursors.WaitCursor;
                 Disassembler.Translator.IncludeAddress = true;
                 Disassembler.Translator.IncludeBinary = true;
                 if (!PeFile.IsPEFile(openFileDialog1.FileName))
@@ -58,21 +136,14 @@ namespace ExeDisassembler
                     return;
                 }
 
-                PeFile _peFile = new PeFile(openFileDialog1.FileName);
-                if (_peFile.Is64Bit)
-                    mode = ArchitectureMode.x86_64;
+                _peFile = new PeFile(openFileDialog1.FileName);
 
-                byte[] b = FileToByteArray(openFileDialog1.FileName, _peFile);
-                var disasm = new Disassembler(b, mode, 0, true);
-                // Disassemble each instruction and output to console
-                richTextBox1.Text = "";
-                List<SharpDisasm.Instruction> dis = disasm.DisassembleList();
 
-                for (int i = 0; i < dis.Count; i++)
-                {
-                    richTextBox1.AppendText(dis[i].ToString() + "\n");
-                }
-
+                readTextSection();
+                readDataSection(".rdata", richTextBox2);
+                readDataSection(".data", richTextBox3);
+                readDataSection(".rsrc", richTextBox2);
+                this.Cursor = Cursors.Default;
 
             }
         }
